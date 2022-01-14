21aa93466d4d2f1f8a58581f2251eb60e813d0fb
tarun wadhwa <pawankwins@gmail.com>
2022-01-14 10:33:45 +0530

    apply filtter on all pages

--- src/app/features/map/map.component.ts
+++ src/app/features/map/map.component.ts
@@ -25,63 +25,11 @@ export class MapComponent implements OnInit {
   lng = 7.809007;
 
 
-
-  customerControl = new FormControl();
-    options: string[] = ['Klevon', 'Aafreen', 'Addison','Lucas','Amelia','Mathias','Marie','Leon','Julia','Maximilian'];
-  filteredOptions: Observable<string[]>;
-
-  countryControl = new FormControl();
-    options1: string[] = ['Afghanistan', 'Japan', 'China','Australia','Bahamas','Brazil','Colombia','India'];
-  filteredOptions1: Observable<string[]>;
-
-  stateControl = new FormControl();
-    options2: string[] = ['State 1', 'State 2', 'State 3','State 4','State 5','State 6','State 7','State 8'];
-  filteredOptions2: Observable<string[]>;
-
-  ipRangeControl = new FormControl();
-    options3: string[] = ['192.168.43.2', '192.168.48.1', '192.168.47.5','192.168.53.2','192.168.91.4','192.168.83.6','192.168.76.1'];
-  filteredOptions3: Observable<string[]>;
-
-
  ngOnInit(): void {
-  this.filteredOptions = this.customerControl.valueChanges.pipe(
-    startWith(''),
-    map(value => this._filter(value)),
-  );
-
-  this.filteredOptions1 = this.countryControl.valueChanges.pipe(
-    startWith(''),
-    map(value => this._filter1(value)),
-  );
-
-  this.filteredOptions2 = this.stateControl.valueChanges.pipe(
-    startWith(''),
-    map(value => this._filter2(value)),
-  );
-  this.filteredOptions3 = this.ipRangeControl.valueChanges.pipe(
-    startWith(''),
-    map(value => this._filter3(value)),
-  );
 
  }
 
 
- private _filter(value: string): string[] {
-  const filterValue = value.toLowerCase();
-  return this.options.filter(option => option.toLowerCase().includes(filterValue));
- }
- private _filter1(value: string): string[] {
-  const filterValue = value.toLowerCase();
-  return this.options1.filter(option => option.toLowerCase().includes(filterValue));
- }
- private _filter2(value: string): string[] {
-  const filterValue = value.toLowerCase();
-  return this.options2.filter(option => option.toLowerCase().includes(filterValue));
- }
- private _filter3(value: string): string[] {
-  const filterValue = value.toLowerCase();
-  return this.options3.filter(option => option.toLowerCase().includes(filterValue));
- }
 
 
  constructor(public dialog: MatDialog){}
--- src/app/shared/components/header-filter/header-filter.component.ts
+++ src/app/shared/components/header-filter/header-filter.component.ts
@@ -13,25 +13,41 @@ export class HeaderFilterComponent implements OnInit {
 
   constructor() { }
 
-
-  PriorityList = new FormControl();
-    options: string[] = ['Choose Option 1', 'Choose Option 2', 'Choose Option 3','Choose Option 4','Choose Option 5'];
+  customerControl = new FormControl();
+    options: string[] = ['Klevon', 'Aafreen', 'Addison','Lucas','Amelia','Mathias','Marie','Leon','Julia','Maximilian'];
   filteredOptions: Observable<string[]>;
 
-  AlarmStatus = new FormControl();
-    options1: string[] = ['Choose Option 1', 'Choose Option 2', 'Choose Option 3','Choose Option 4','Choose Option 5'];
+  countryControl = new FormControl();
+    options1: string[] = ['Afghanistan', 'Japan', 'China','Australia','Bahamas','Brazil','Colombia','India'];
   filteredOptions1: Observable<string[]>;
+
+  stateControl = new FormControl();
+    options2: string[] = ['State 1', 'State 2', 'State 3','State 4','State 5','State 6','State 7','State 8'];
+  filteredOptions2: Observable<string[]>;
+
+  ipRangeControl = new FormControl();
+    options3: string[] = ['192.168.43.2', '192.168.48.1', '192.168.47.5','192.168.53.2','192.168.91.4','192.168.83.6','192.168.76.1'];
+  filteredOptions3: Observable<string[]>;
 
 
   ngOnInit(): void {
-    this.filteredOptions = this.PriorityList.valueChanges.pipe(
+    this.filteredOptions = this.customerControl.valueChanges.pipe(
       startWith(''),
       map(value => this._filter(value)),
     );
 
-    this.filteredOptions1 = this.AlarmStatus.valueChanges.pipe(
+    this.filteredOptions1 = this.countryControl.valueChanges.pipe(
       startWith(''),
       map(value => this._filter1(value)),
+    );
+
+    this.filteredOptions2 = this.stateControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter2(value)),
+    );
+    this.filteredOptions3 = this.ipRangeControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter3(value)),
     );
 
   }
@@ -43,6 +59,14 @@ export class HeaderFilterComponent implements OnInit {
    private _filter1(value: string): string[] {
     const filterValue = value.toLowerCase();
     return this.options1.filter(option => option.toLowerCase().includes(filterValue));
+   }
+   private _filter2(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.options2.filter(option => option.toLowerCase().includes(filterValue));
+   }
+   private _filter3(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.options3.filter(option => option.toLowerCase().includes(filterValue));
    }
 
 }
